40c80d4572f044f090a643d19edd84635a302e22
rimiti <dimitri.dobairro@dimsolution.com>
2017-12-05 18:44:47 +0100

    chore(__tests__/components/toastify.js) test context created

---
+++ __tests__/components/toastify.js
@@ -0,0 +1,9 @@
+import 'react-native';
+import React from 'react';
+import { shallow } from 'enzyme';
+import Toastify from '../../src';
+
+describe('Toastify', () => {
+  it('Renders default state', () => {
+  });
+});
