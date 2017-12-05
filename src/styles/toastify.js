4c34f780186db3389c31198100566cb2307aeddc
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2017-12-05 23:26:26 +0100

    chore(styles/toastify.js) style created

--- src/styles/toastify.js
+++ src/styles/toastify.js
@@ -1,4 +1,20 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
+  container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    elevation: 999,
+    alignItems: 'center',
+    zIndex: 10000,
+  },
+  content: {
+    backgroundColor: '#000',
+    borderRadius: 5,
+    padding: 10,
+  },
+  text: {
+    color: '#fff',
+  },
 });
