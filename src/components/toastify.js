c258a8199f5980e9bf04f12c059c78065ac6ee88
rimiti <dimitri.dobairro@dimsolution.com>
2017-12-05 18:58:31 +0100

    feat(components/toastify.js) main component structure created

---
+++ src/components/toastify.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { View, Modal, Text, ScrollView, TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
+import styles from '../styles/toastify';
+
+export default class Tostify extends Component {
+
+}
