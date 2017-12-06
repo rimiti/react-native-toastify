edc7246732a79f10ddb70e5ef612473eb38c7ef6
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2017-12-07 00:16:18 +0100

    chore(__tests__/components/toastify.js) test in progress

--- __tests__/components/toastify.js
+++ __tests__/components/toastify.js
@@ -4,7 +4,7 @@ import { shallow } from 'enzyme';
 import Toastify from '../../src';
 
 function factory(props = {}) {
-  const wrapper = shallow(<Toastify {...props} />, { context: {} });
+  const wrapper = shallow(<Toastify ref="toastify" {...props} />, { context: {} });
   return {
     props,
     wrapper,
@@ -14,6 +14,16 @@ function factory(props = {}) {
 describe('Toastify', () => {
   it('Renders default state', () => {
     const { wrapper } = factory();
+    expect(wrapper.type()).toBe(null);
+  });
+
+  it('Test in progress', () => {
+    const { wrapper } = factory();
+    wrapper.instance().show('test', 10000);
+    wrapper.instance().show('test', '10000');
+    wrapper.instance().close(10000);
+    wrapper.instance().close('10000');
+    wrapper.instance().getPosition();
     expect(wrapper.type()).toBe(null);
   });
 });
