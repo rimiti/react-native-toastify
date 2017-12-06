8fd72198cca193a640c5310058b6188359d617ba
rimiti <dimitri.dobairro@dimsolution.com>
2017-12-06 18:18:42 +0100

    feat(components/toastify.js) propTypes created

--- src/components/toastify.js
+++ src/components/toastify.js
@@ -1,11 +1,33 @@
-import React, {Component} from 'react';
-import {View, Text, Animated, Dimensions} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Animated, Dimensions } from 'react-native';
 import PropTypes from 'prop-types';
 import styles from '../styles/toastify';
 
 export default class Toastify extends Component {
+  static propTypes = {
+    style: View.propTypes.style,
+    position: PropTypes.oneOf([
+      'top',
+      'center',
+      'bottom',
+    ]),
+    textStyle: Text.propTypes.style,
+    positionValue: PropTypes.number,
+    fadeInDuration: PropTypes.number,
+    fadeOutDuration: PropTypes.number,
+    opacity: PropTypes.number,
+  };
 
-  constructor(props) {
-    super(props);
+  static defaultProps = {
+    position: 'bottom',
+    textStyle: styles.text,
+    positionValue: 120,
+    fadeInDuration: 500,
+    fadeOutDuration: 500,
+    opacity: 1,
+  };
+
+  constructor() {
+    super();
   }
 }
