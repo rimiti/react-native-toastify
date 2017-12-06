d6b9e49bdd42187caf434b07d9b01312eb227399
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2017-12-06 23:51:14 +0100

    chore(__tests__/components/toastify.js) tests use a factory function

--- __tests__/components/toastify.js
+++ __tests__/components/toastify.js
@@ -3,9 +3,17 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Toastify from '../../src';
 
+function factory(props = {}) {
+  const wrapper = shallow(<Toastify {...props} />, { context: {} });
+  return {
+    props,
+    wrapper,
+  };
+}
+
 describe('Toastify', () => {
   it('Renders default state', () => {
-    const wrapper = shallow(<Toastify />);
+    const { wrapper } = factory();
     expect(wrapper.type()).toBe(null);
   });
 });
