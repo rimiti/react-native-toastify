efd0371d309117f7c92f588a78cfcc46b23e6062
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2017-12-08 19:16:12 +0100

    chore(__tests__/components/toastify.js) maximum coverage

--- __tests__/components/toastify.js
+++ __tests__/components/toastify.js
@@ -6,7 +6,7 @@ import Toastify from '../../src';
 function factory(props = {}) {
   return {
     props,
-    wrapper: shallow(<Toastify ref="toastify" {...props} />, { context: {} }),
+    wrapper: shallow(<Toastify {...props} />, { context: {} }),
   };
 }
 
@@ -23,6 +23,7 @@ describe('Toastify', () => {
       opacity: 1,
       position: 'bottom',
       positionValue: 120,
+      style: {},
       textStyle: { color: '#fff' },
     };
     expect(wrapper.type()).toBe(null);
@@ -35,13 +36,42 @@ describe('Toastify', () => {
     expect(wrapper.state('isShow')).toEqual(true);
     expect(wrapper.state('text')).toEqual('Hello word !');
     expect(wrapper.state('duration')).toEqual(10000);
+    expect(wrapper.instance().resetTimeout());
   });
 
-  it('Render with hiding state', () => {
+  it('Render with hiding state with configuration 1', () => {
     const { wrapper } = factory();
-    wrapper.instance().close();
+    wrapper.instance().close(10);
     expect(wrapper.state('isShow')).toEqual(false);
     expect(wrapper.state('text')).toEqual('');
+  });
+
+  it('Render with hiding state with configuration 2', () => {
+    const { wrapper } = factory();
+    wrapper.instance().show('Hello word !', 100000);
+    expect(wrapper.state('isShow')).toEqual(true);
+    expect(wrapper.state('text')).toEqual('Hello word !');
+    wrapper.instance().close(0);
+    expect(wrapper.state('isShow')).toEqual(true);
+  });
+
+  it('Render with hiding (no show delay)', () => {
+    const { wrapper } = factory();
+    wrapper.instance().show('Hello word !');
+    expect(wrapper.state('isShow')).toEqual(true);
+    expect(wrapper.state('text')).toEqual('Hello word !');
+    wrapper.instance().close(10);
+    expect(wrapper.state('isShow')).toEqual(true);
+  });
+
+  it('Render with hiding (no show and close delay)', (done) => {
+    const { wrapper } = factory({ fadeOutDuration: 0, fadeInDuration: 0 });
+    wrapper.instance().show('Hello word !');
+    expect(wrapper.state('isShow')).toEqual(true);
+    expect(wrapper.state('text')).toEqual('Hello word !');
+    wrapper.instance().close();
+    expect(wrapper.state('isShow')).toEqual(true);
+    setTimeout(() => done(), 1000);
   });
 
   it('Test position method (top)', () => {
@@ -52,6 +82,11 @@ describe('Toastify', () => {
   it('Test position method (bottom)', () => {
     const { wrapper } = factory({ position: 'bottom' });
     expect(wrapper.instance().position()).toEqual(1214);
+  });
+
+  it('Test position method (center)', () => {
+    const { wrapper } = factory({ position: 'center' });
+    expect(wrapper.instance().position()).toEqual(667);
   });
 
   it('Test position method (center)', () => {
