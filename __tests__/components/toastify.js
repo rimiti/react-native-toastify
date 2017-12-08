474e78d76a8e49410901ba9e2b0fe5a4009ec1a9
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2017-12-08 13:05:31 +0100

    chore(__tests__/components/toastify.js) tests added

--- __tests__/components/toastify.js
+++ __tests__/components/toastify.js
@@ -13,23 +13,49 @@ function factory(props = {}) {
 describe('Toastify', () => {
   it('Render with default state', () => {
     const { wrapper } = factory();
+    const expected = {
+      defaultCloseDelay: 250,
+      durationLong: 2000,
+      durationShort: 500,
+      end: 0,
+      fadeInDuration: 500,
+      fadeOutDuration: 500,
+      opacity: 1,
+      position: 'bottom',
+      positionValue: 120,
+      textStyle: { color: '#fff' },
+    };
     expect(wrapper.type()).toBe(null);
+    expect(wrapper.instance().props).toEqual(expected);
   });
 
-  it('Render with open state', () => {
+  it('Render when toast is showing', () => {
     const { wrapper } = factory();
-    wrapper.instance().show(10000);
+    wrapper.instance().show('Hello word !', 10000);
+    expect(wrapper.state('isShow')).toEqual(true);
+    expect(wrapper.state('text')).toEqual('Hello word !');
+    expect(wrapper.state('duration')).toEqual(10000);
   });
 
-  // it('Render with close state', () => {
-  //   const { wrapper } = factory();
-  //   wrapper.instance().show('test', 10000);
-  //   expect(wrapper.type()).toBe(null);
-  // });
-  //
-  // it('Test position method', () => {
-  //   const { wrapper } = factory();
-  //   wrapper.instance().position();
-  //   expect(wrapper.type()).toBe(null);
-  // });
+  it('Render with hiding state', () => {
+    const { wrapper } = factory();
+    wrapper.instance().close();
+    expect(wrapper.state('isShow')).toEqual(false);
+    expect(wrapper.state('text')).toEqual('');
+  });
+
+  it('Test position method (top)', () => {
+    const { wrapper } = factory({ position: 'top' });
+    expect(wrapper.instance().position()).toEqual(120);
+  });
+
+  it('Test position method (bottom)', () => {
+    const { wrapper } = factory({ position: 'bottom' });
+    expect(wrapper.instance().position()).toEqual(1214);
+  });
+
+  it('Test position method (center)', () => {
+    const { wrapper } = factory({ position: 'center' });
+    expect(wrapper.instance().position()).toEqual(667);
+  });
 });
