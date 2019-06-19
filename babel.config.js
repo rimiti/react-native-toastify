814e2e1f1b5703f1c03b98187490cd1cb9092f8e
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-06-19 10:53:15 +0200

    feat(babel.config.js) configuration updated

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,9 @@
 module.exports = {
   "presets": [
+    "@babel/preset-env",
     "react-native"
   ],
-  "env": {
-    "production": {
-      "comments": false,
-      "minified": true
-    }
-  },
   "plugins": [
     "@babel/plugin-proposal-class-properties"
   ]
-}
+};
