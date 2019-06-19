8a0f220f2c66abf8782a999ddc42fd6876af9d15
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-06-19 11:18:02 +0200

    style(components/toastify.js) auto lint

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   presets: [
-    "@babel/preset-env",
-    "module:metro-react-native-babel-preset"
+    '@babel/preset-env',
+    'module:metro-react-native-babel-preset',
   ],
   plugins: [
-    "@babel/plugin-proposal-class-properties"
-  ]
+    '@babel/plugin-proposal-class-properties',
+  ],
 };
